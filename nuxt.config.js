25baab9fbd78bfa59586f2021d5ae1d186381cb3
Maki <loudnownet@gmail.com>
2020-06-29 14:24:07 +0200

    fix favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'We help forward-thinking companies to build digital experiences.' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ],
   },
   /*
