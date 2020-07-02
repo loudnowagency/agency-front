9ea1fa4dbfa21c9562abdd5f9d41c6891e367740
Maki <loudnownet@gmail.com>
2020-07-02 15:04:42 +0200

    add Logo component

---
+++ components/ui/Logo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="logo">
+    <InlineSvg
+      class="logo__img"
+      :src="src" />
+    <span class="logo__meta">
+      {{ meta }}
+    </span>
+  </div>
+</template>
+
+<script>
+import InlineSvg from 'vue-inline-svg'
+
+export default {
+  name: 'Logo',
+  components: { InlineSvg },
+  props: {
+    src: String,
+    meta: String,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.logo__img {
+  height: 24px;
+}
+
+.logo__meta {
+  display: block;
+  font-family: $font-tech;
+  opacity: .4;
+  font-size: 11px;
+}
+</style>
